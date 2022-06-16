e080090f795ef3c5175f91fd1eb4cffc1acef512
KentaKodama99 <kenta.kodama22jyt22@gmail.com>
2022-06-17 01:40:38 +0900

    abc255_c_2

--- abc255/abc255_c.py
+++ abc255/abc255_c.py
@@ -1,17 +1,29 @@
 import sys
 
-X,A,D,N=map(int,input().split())
-
-if D == 0: # 公差が固定
-    print(abs(A - X))
+X, A, D, N = map(int, input().split())
+if D == 0:
+    print(abs(X - A))
     sys.exit()
-m = A + (N - 1) * D # 末項
-ans = min(abs(A - X), abs(m - X)) # 初項、末項とXの差の最小値
-# print(ans)
-y = A + (X - A) // D * D # A + |(X - A) / D| * D
-print(y)
-for i in range(-2, 3): # 周辺±2程度探索
-    z = y + i * D
-    if A <= z <= m or m <= z <= A: # 探索範囲内に存在するかどうか
-        ans = min(ans, abs(z - X))
+# 公差の符号によって大小関係の正規化
+elif D < 0:
+    st, fi = A + (N - 1) * D, A
+    D = -D
+else:
+    st, fi = A, A + (N - 1) * D
+    
+f = lambda n : st + (n - 1) * D
+# 二分探索で最小距離更新
+r = 1
+l = N
+ans = min(abs(st - X), abs(fi - X))
+
+while abs(r - l) > 1:
+    c = (r + l) // 2
+    if st <= f(c) <= fi or fi <= f(c) <= st:
+        ans = min(ans, abs(X - f(c)))
+    if f(c) <= X:
+        r = c
+    else:
+        l = c
+
 print(ans)
